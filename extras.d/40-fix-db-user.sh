62c98c3af94b392bd120fa21ccf3d6f1aecb3210
Joshua Harlow <jxharlow@godaddy.com>
2016-09-15 21:41:20 -0700

    Start and restart mariadb before doing further work

--- extras.d/40-fix-db-user.sh
+++ extras.d/40-fix-db-user.sh
@@ -2,8 +2,10 @@
 
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
     if is_service_enabled mysql; then
+        start_service mariadb
         sudo mysqladmin -u root password $DATABASE_PASSWORD || true
         sudo mysql -uroot -p$DATABASE_PASSWORD -h127.0.0.1 -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'$MYSQL_HOST' identified by '$MYSQL_PASSWORD';"
         sudo mysql -uroot -p$DATABASE_PASSWORD -h127.0.0.1 -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' identified by '$MYSQL_PASSWORD';"
+        restart_service mariadb
     fi
 fi
