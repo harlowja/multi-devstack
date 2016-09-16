712080a15d5eabc562c7499a2b2e8684e494e659
Joshua Harlow <jxharlow@godaddy.com>
2016-09-15 22:34:20 -0700

    Add echo when privileges being fixed

--- extras.d/40-fix-db-user.sh
+++ extras.d/40-fix-db-user.sh
@@ -2,6 +2,7 @@
 
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
     if is_service_enabled mysql; then
+        echo "Fixing mysql user privileges..."
         start_service mariadb
         sudo mysqladmin -u root password $DATABASE_PASSWORD || true
         sudo mysql -uroot -p$DATABASE_PASSWORD -h127.0.0.1 -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'$MYSQL_HOST' identified by '$MYSQL_PASSWORD';"
