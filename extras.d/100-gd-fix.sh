718dfd3947f45f880496206a729f6c7bcc012ff2
Joshua Harlow <jxharlow@godaddy.com>
2016-09-15 17:15:32 -0700

    Add reference to upstream fix

--- extras.d/100-gd-fix.sh
+++ extras.d/100-gd-fix.sh
@@ -1,5 +1,7 @@
 # This is fixed in mitaka and newer, but for now we do this to make
 # sure the driver actually gets installed...
+#
+# See: https://github.com/openstack-dev/devstack/commit/7dd890d6e13
 
 if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
     if [ -n "$DATABASE_TYPE" ]; then
