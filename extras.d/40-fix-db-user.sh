1db5814928d42c855ef1bd09004ea80bc3c870f2
Joshua Harlow <jxharlow@godaddy.com>
2016-09-15 21:37:22 -0700

    End mysql statements with ;

--- extras.d/40-fix-db-user.sh
+++ extras.d/40-fix-db-user.sh
@@ -1,8 +1,9 @@
 # Something is busted with mysql and the default user, so we'll just fix it..
+
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
     if is_service_enabled mysql; then
         sudo mysqladmin -u root password $DATABASE_PASSWORD || true
-        sudo mysql -uroot -p$DATABASE_PASSWORD -h127.0.0.1 -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'$MYSQL_HOST' identified by '$MYSQL_PASSWORD'"
-        sudo mysql -uroot -p$DATABASE_PASSWORD -h127.0.0.1 -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' identified by '$MYSQL_PASSWORD'"
+        sudo mysql -uroot -p$DATABASE_PASSWORD -h127.0.0.1 -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'$MYSQL_HOST' identified by '$MYSQL_PASSWORD';"
+        sudo mysql -uroot -p$DATABASE_PASSWORD -h127.0.0.1 -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' identified by '$MYSQL_PASSWORD';"
     fi
 fi
