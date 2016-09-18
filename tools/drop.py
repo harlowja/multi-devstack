9046857cb83b44227eb204208d6d0555ebc4613b
Joshua Harlow <jxharlow@godaddy.com>
2016-09-18 01:10:06 -0700

    Add new dump tool and rename del_stage

---
+++ tools/drop.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import sys
+
+import sqlitedict
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--state",
+                        help="file to read/write action state"
+                             " information into/from (default=%(default)s)",
+                        default=os.path.join(os.getcwd(), "state.sqlite"),
+                        metavar="PATH")
+    parser.add_argument("-f", "--func",
+                        help="func/stage to delete/drop",
+                        action='append', default=[])
+    parser.add_argument("-c", "--command",
+                        help="command name to delete/drop",
+                        default=[], action='append')
+    parser.add_argument("-t", "--table",
+                        help="table name to use",
+                        default=None, required=True)
+    args = parser.parse_args()
+    with sqlitedict.SqliteDict(filename=args.state, flag='c',
+                               tablename=args.table,
+                               autocommit=False) as tracker:
+        maybe_servers = tracker.get("maybe_servers", [])
+        for server_name in maybe_servers:
+            try:
+                record = tracker[server_name]
+            except KeyError:
+                pass
+            else:
+                for f in args.func:
+                    record.funcs.discard(f)
+                for c in args.command:
+                    record.cmds.discard(c)
+                tracker[server_name] = record
+                tracker.sync()
+
+
+if __name__ == '__main__':
+    main()
--- tools/dump.py
+++ tools/dump.py
@@ -1,8 +1,19 @@
 import argparse
+import pprint as pp
 import os
 import sys
 
+import munch
 import sqlitedict
+
+possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
+                                   os.pardir,
+                                   os.pardir))
+
+if os.path.exists(os.path.join(possible_topdir,
+                               'builder',
+                               '__init__.py')):
+    sys.path.insert(0, possible_topdir)
 
 
 def main():
@@ -12,24 +23,14 @@ def main():
                              " information into/from (default=%(default)s)",
                         default=os.path.join(os.getcwd(), "state.sqlite"),
                         metavar="PATH")
-    parser.add_argument("-s", "--stage",
-                        help="stage name to delete/drop",
-                        default=None, required=True)
     parser.add_argument("-t", "--table",
                         help="table name to use",
                         default=None, required=True)
     args = parser.parse_args()
-    with sqlitedict.SqliteDict(filename=args.state, flag='c',
+    with sqlitedict.SqliteDict(filename=args.state, flag='r',
                                tablename=args.table,
                                autocommit=False) as tracker:
-        try:
-            del tracker[args.stage]
-        except KeyError:
-            print("Stage '%s' not found." % args.stage)
-            sys.exit(1)
-        else:
-            print("Stage '%s' removed." % args.stage)
-            tracker.sync()
+        pp.pprint(munch.unmunchify(dict(tracker)))
 
 
 if __name__ == '__main__':
